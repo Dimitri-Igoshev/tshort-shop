984a9a0a28498ab99f8827cc86fbce1ca56d43a9
Dimitri-Igoshev <d.o.igoshev@gmail.com>
2023-04-06 16:13:40 +0200

    Canvas

--- src/canvas/Canvas.jsx
+++ src/canvas/Canvas.jsx
@@ -1,5 +1,26 @@
-export const Canvas = () => {
+import { Canvas } from '@react-three/fiber'
+import { Center, Environment } from '@react-three/drei'
+import { CameraRig } from './CameraRig'
+import { Backdrop } from './Backdrop'
+import { Shirt } from './Shirt'
+
+export const CanvasModel = () => {
   return (
-    <div></div>
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full h-full transition-all ease-in"
+    >
+      <ambientLight intensity={0.5}/>
+      <Environment preset="city"/>
+
+      <CameraRig>
+        <Backdrop/>
+        <Center>
+          <Shirt/>
+        </Center>
+      </CameraRig>
+    </Canvas>
   )
 }
--- src/canvas/index.js
+++ src/canvas/index.js
@@ -1,1 +1,4 @@
 export * from './Canvas.jsx'
+export * from './Backdrop.jsx'
+export * from './CameraRig.jsx'
+export * from './Shirt.jsx'
